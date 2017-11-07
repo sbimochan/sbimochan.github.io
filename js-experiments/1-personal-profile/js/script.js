98e713df44bad13029613158f8e64568572100c8
sbimochan <bmochan@gmail.com>
2017-11-07 21:05:55 +0545

    json profile

---
+++ js-experiments/1-personal-profile/js/script.js
@@ -0,0 +1,12 @@
+var profile = {
+    name: "Bimochan Shrestha",
+    dob: "1994-09-12",
+    college: "Himalaya College of Engineering",
+    location: "Shantinagar",
+    github: "sbimochan"
+}
+document.getElementById('name').innerHTML=profile.name;
+document.getElementById('dob').innerHTML=profile.dob;
+document.getElementById('college').innerHTML=profile.college;
+document.getElementById('location').innerHTML=profile.location;
+document.getElementById('github').innerHTML=profile.github;
