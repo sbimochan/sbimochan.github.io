d42e5f9a45f2abfdcaaa6a21e42373761beaf319
sbimochan <bmochan@gmail.com>
2017-11-07 22:44:49 +0545

    age, profile pic and link

--- js-experiments/1-personal-profile/js/script.js
+++ js-experiments/1-personal-profile/js/script.js
@@ -1,12 +1,31 @@
 var profile = {
     name: "Bimochan Shrestha",
+    img:"https://avatars3.githubusercontent.com/u/11685953?s=400&u=327ce2ce4cce6ed6fc3c7368ee1be781916b04f0&v=4",
     dob: "1994-09-12",
     college: "Himalaya College of Engineering",
     location: "Shantinagar",
-    github: "sbimochan"
+    github: "https://github.com/sbimochan"
 }
-document.getElementById('name').innerHTML=profile.name;
-document.getElementById('dob').innerHTML=profile.dob;
-document.getElementById('college').innerHTML=profile.college;
-document.getElementById('location').innerHTML=profile.location;
-document.getElementById('github').innerHTML=profile.github;
+//Age Calculation
+var dob=profile.dob;
+var splits=dob.split('-');
+var today=new Date();
+var year=today.getFullYear();
+var age=year-splits[0];
+//writing into span
+document.getElementById('name').innerHTML = profile.name;
+document.getElementById('dob').innerHTML = profile.dob;
+document.getElementById('age').innerHTML = age;
+document.getElementById('college').innerHTML = profile.college;
+document.getElementById('location').innerHTML = profile.location;
+document.getElementById('github').innerHTML = profile.github;
+
+//creating link
+var a = document.createElement('a');
+var portfolio=document.getElementsByClassName("portfolio");
+var linkText = document.createTextNode("Github");
+a.appendChild(linkText);
+a.href = profile.github;
+portfolio[0].appendChild(a);
+//for profile pic
+document.getElementById("profPic").src="https://avatars3.githubusercontent.com/u/11685953?s=400&u=327ce2ce4cce6ed6fc3c7368ee1be781916b04f0&v=4";
--- js-experiments/1-scatter-plotting/script.js
+++ js-experiments/1-scatter-plotting/script.js
@@ -1,7 +1,7 @@
 var wrapper = document.getElementById("wrapper");
 var box = document.createElement("div");
-var ul=document.createElement("ul");
-ul.style.paddingTop="502px";
+var ul = document.createElement("ul");
+ul.style.paddingTop = "502px";
 box.appendChild(ul);
 box.style.position = "relative";
 box.id = "box";
@@ -21,14 +21,14 @@ var i = 0;
 do {
     _top = getRandomInt(1, 490);
     _left = getRandomInt(1, 490);
-  
+
     var dict = {
         top: _top,
         left: _left
     }
     var stars = document.createElement("div");
     stars.id = "stars" + i;
-    stars.className="stars";
+    stars.className = "stars";
     stars.style.width = "10px";
     stars.style.height = "10px";
     stars.style.position = "absolute";
@@ -38,13 +38,12 @@ do {
     box.appendChild(stars);
     i++;
     data.push(dict);
-    stars.onclick=function(){
-    	var removed=box.removeChild(this);
-    	var listElements = document.createElement("li");
-    	ul.appendChild(listElements);
-    	var __top=removed.style.getPropertyValue('top');
-    	var __left=removed.style.getPropertyValue('left');
-    	listElements.innerHTML = "top: "+__top+" left: "+__left; 
+    stars.onclick = function() {
+        var removed = box.removeChild(this);
+        var listElements = document.createElement("li");
+        ul.appendChild(listElements);
+        var __top = removed.style.getPropertyValue('top');
+        var __left = removed.style.getPropertyValue('left');
+        listElements.innerHTML = "top: " + __top + " left: " + __left;
     }
 } while (i < 20);
-
