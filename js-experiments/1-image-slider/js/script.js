b1886feaa0be1b742a239645c33624ba499446e3
sbimochan <bmochan@gmail.com>
2017-11-08 19:25:27 +0545

    working slide

--- js-experiments/1-image-slider/js/script.js
+++ js-experiments/1-image-slider/js/script.js
@@ -1,17 +1,106 @@
+var mainWrapper = document.getElementById('wrapper');
+var imageContainer = document.createElement('ul');
+var imageViewer = document.createElement("div");
+imageViewer.id="container";
+
+var posLeft = 0;
+
+mainWrapper.appendChild(imageViewer);
 var next = document.getElementById('next');
 var prev = document.getElementById('prev');
-var images = document.getElementsByClassName('img');
-// alert(images.length);
-// console.log(images);
-var i = 1;
-for (var j = 1; j <= images.length; j++) {
-    var className = "img" + j;
-    // console.log(className);
-    var className = document.getElementsByClassName(className);
-    // console.log(className);
-    className.style.zindex="1";
+
+imageViewer.appendChild(imageContainer);
+imageViewer.style.width = "960px";
+imageViewer.style.height = "420px";
+imageViewer.style.overflow = "hidden";
+imageViewer.style.liststyle = "none";
+imageViewer.style.position = "relative";
+imageContainer.style.position = "absolute";
+imageContainer.style.width = "4000px";
+
+/*Counter*/
+var imgNumber = 0;
+
+// images url
+
+var images = ["images/image-slider-1.jpg", "images/image-slider-2.jpg", "images/image-slider-3.jpg", "images/image-slider-4.jpg"];
+
+/*function for adding images from array*/
+var addImg = function(imgName) {
+    var image = document.createElement('img');
+    image.src = imgName;
+    var list = document.createElement('li');
+    list.style.display = "inline-block";
+    list.appendChild(image);
+    imageContainer.appendChild(list);
+
 }
-    next.onclick = function() {
-        // z-index=i++;
-        // console.log(className);
+//add image to loop
+for (var x = 0; x < images.length; x++) {
+    addImg(images[x]);
+}
+//function for setting image loop---------------------------------------------------------------------
+function check(value) {
+    if (value > 3) {
+        return 0;
     }
+
+    if (value < 0) {
+        return 3;
+    }
+
+    return value;
+}
+
+function animateNext(percent) {
+
+    var stopper = setInterval(function() {
+        percent -= 2.5;
+        console.log('next', percent);
+        imageContainer.style.left = percent + "%";
+        if (Math.abs(percent % 100) == 0) {
+            clearInterval(stopper);
+        }
+    }, 25);
+}
+
+function animatePrev(percent) {
+    var stopper = setInterval(function() {
+        percent += 2.5;
+        console.log('prev', percent);
+        imageContainer.style.left = percent + "%";
+        if (Math.abs(percent % 100) == 0) {
+            clearInterval(stopper);
+        }
+    }, 25);
+}
+//function to prevent more than one click---------------------------------------------------------------------------------
+var flag = true;
+
+function preventClick() {
+    setTimeout(function() {
+        flag = true;
+    }, 1000);
+}
+next.onclick = function() {
+    if (flag == true) {
+        flag = false;
+
+        imgNumber++; //2
+        imgNumber = check(imgNumber);
+        var percent = -(imgNumber - 1) * 100;
+        console.log('newnext', imgNumber);   
+        animateNext(percent);
+        preventClick();
+    }
+}
+prev.onclick = function() {
+    if (flag == true) {
+        flag = false;
+        imgNumber--;
+        imgNumber = check(imgNumber);
+        var percent = -(imgNumber + 1) * 100;
+        animatePrev(percent);
+        preventClick();
+    }
+}
