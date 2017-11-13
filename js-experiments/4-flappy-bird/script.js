c3138e8cb7679cf51fa593fc73a733883004466e
sbimochan <bmochan@gmail.com>
2017-11-13 14:41:44 +0545

    some improvements

--- js-experiments/4-flappy-bird/script.js
+++ js-experiments/4-flappy-bird/script.js
@@ -1,17 +1,29 @@
-class World{
-  constructor(x,dx){
-    this.x=1;
-    this.dx=2;
-    this.worldElement=document.getElementById('bg');
-
-    this.updatePosition = function(){
-      this.x=this.x+this.dx;
-      this.worldElement.style.backgroundPositionX="right "+this.x+"px";
-    };    
+class World {
+  constructor(elem) {
+    this.x = 1;
+    this.dx = 2;
+    this.worldElement = elem;
+  }
+  updatePosition() {
+    this.x += this.dx;
+    this.worldElement.style.backgroundPositionX = "right " + this.x + "px";
   }
 }
-let world = new World;
-let updateInterval=setInterval(function(){
+let elem = document.getElementById("bg");
+let world = new World(elem);
+let updateInterval = setInterval(function() {
   world.updatePosition();
-},10);
+}, 10);
 
+class Bird {
+  constructor(elem) {
+    this.birdElement = document.createElement("img");
+    this.birdElement.src = "images/bird.png";
+    this.birdElement.style.top = "220px";
+    this.birdElement.style.height = "100px";
+    this.birdElement.style.width = "100px";
+    this.birdElement.style.position = "absolute";
+    elem.appendChild(this.birdElement);
+  }
+}
+let bird = new Bird(elem);
