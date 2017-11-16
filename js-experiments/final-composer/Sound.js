1d7bb2b77deba9b9af9bb44e83911eca1c99098a
sbimochan <bmochan@gmail.com>
2017-11-16 19:16:57 +0545

    json issue

---
+++ js-experiments/final-composer/Sound.js
@@ -0,0 +1,100 @@
+class Sound {
+  constructor(context) {
+    this.context = context; //feed AudioContext. webkit AudioContext for Safari
+  }
+  init() {
+    this.oscillator = this.context.createOscillator(); //for electronic math sound
+
+    this.gainNode = this.context.createGain(); //for gain
+
+    this.oscillator.connect(this.gainNode); //transferring to speakers
+    this.gainNode.connect(this.context.destination);
+    this.oscillator.type = "traingle"; //could be waveforms like sine,square,triangle,sawtooth
+  }
+  // init();
+  //Creating play method
+  play(hertz, time, cents) {
+    //generate tone eg play(261.63, now);
+
+    this.init();
+    this.oscillator.frequency.value = hertz;
+    this.oscillator.detune.value = cents;
+    this.gainNode.gain.setValueAtTime(1, this.context.currentTime); //currentTime is 2x accurate than Date
+    this.oscillator.start(time);
+    this.stop(time);
+  }
+  stop(time) {
+    this.gainNode.gain.exponentialRampToValueAtTime(0.01, time + 1);
+    this.oscillator.stop(time + 1);
+  }
+}
+let context = new (window.AudioContext || window.webkitAudioContext)();
+let note = new Sound(context);
+// note.init();
+
+// note.play(261.63, now + 2);
+// note.play(293.66, now + 2 + 0.5);
+// note.play(329.63, now + 2 + 1);
+// note.play(349.23, now + 2 + 1.5);
+// note.play(392.0, now + 2 + 2);
+// note.play(440.0, now + 2 + 2.5);
+// note.play(493.88, now + 2 + 3);
+// note.play(523.25, now + 2 + 3.5);
+// function playScale() {
+  // let now = context.currentTime;
+  // console.log("played")
+  // note.play(hertzFinder(), now, -798);
+  // note.play(261.63, now + 2);
+  // note.play(293.66, now + 2 + 0.5);
+  // note.play(329.63, now + 2 + 1);
+  // note.play(349.23, now + 2 + 1.5);
+  // note.play(392.0, now + 2 + 2);
+  // note.play(440.0, now + 2 + 2.5);
+  // note.play(493.88, now + 2 + 3);
+  // note.play(523.25, now + 2 + 3.5);
+// }
+function stopScale() {
+  note.stop(0);
+}
+let hertz = {
+  C4: 261.63,
+  D4: 293.66,
+  E4: 329.63,
+  F4: 349.23,
+  G4: 392.0,
+  A4: 440.0,
+  B4: 493.88
+};
+
+let toneSelector = document.getElementById("noteId");
+let composedNotes = [];
+
+function collectNotes(){
+    let hertzValue = toneSelector.options[toneSelector.selectedIndex].value;
+    composedNotes.push(hertzValue);
+    hertzFinder();
+    // console.log(composedNotes);
+}
+function hertzFinder(){
+  composedNotes.forEach(function(composedNote){
+    console.log(composedNote);
+    let hertzValue = hertz.composedNote;
+    // console.log("hertzvalue", hertz.composedNote);
+    // return hertzValue;
+  });
+}
+function playScale(){
+   let now = context.currentTime;
+   note.play(hertzFinder(), now, -798);
+  //  console.log(hertzFinder());
+}
+
+// setInterval(playScale, 1000);
+
+// class Section{
+//   constructor(){
+//     this.noteValue = noteValue;
+
+//   }
+// }
+// setTimeout(playScale,3000);
--- js-experiments/final-composer/script.js
+++ js-experiments/final-composer/script.js
@@ -1,7 +1,77 @@
-let audio = new Audio("tones/Classical_guitar_scale.ogg");
-function playScale(){
-  audio.play();
+// let audio = new Audio("tones/Classical_guitar_scale.ogg");
+// function playScale(){
+//   audio.play();
+// }
+// function stopScale(){
+//   audio.pause();
+// }
+
+// var context = new (window.AudioContext || window.webkitAudioContext)();
+
+// var oscillator = context.createOscillator();
+
+// oscillator.type = "sine";
+// oscillator.frequency.value = 440;
+// oscillator.connect(context.destination);
+// oscillator.start();
+
+
+
+let context = new AudioContext();
+let o = context.createOscillator();
+let g = context.createGain();
+let filter = context.createBiquadFilter();
+
+
+o.type="square";;
+g.type="sine"; //it can be sine,square,triangle,sawtooth
+o.connect(context.destination);
+filter.type=filter.lowpass;
+filter.frequency.value=440;
+filter.connect(context.destination);
+// o.connect(g);
+// g.connect(context.destination);
+// g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 0.04);
+
+function playScale() {
+  // o.start();
+  filter.start();
 }
-function stopScale(){
-  audio.pause();
+function stopScale() {
+  o.stop();
 }
+function gainScale(){
+
+}
+let sounds = [
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/G4.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/A4.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/C5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/D5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/E5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/G5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/A5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/C6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/D6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/D%236.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/E6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/G6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/A6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/C7.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/D7.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_G4.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_A4.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_C5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_D5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_E5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_G5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_A5.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_C6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_D6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_D%236.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_E6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_G6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_A6.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_C7.mp3",
+  "https://s3-us-west-2.amazonaws.com/s.cdpn.io/355309/d_D7.mp3"
+];
