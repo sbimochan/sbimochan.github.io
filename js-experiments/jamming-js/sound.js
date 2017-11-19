c23fe01eb8e6710fdcb46bf4d1987f5e7281dee7
sbimochan <bmochan@gmail.com>
2017-11-19 19:53:37 +0545

    toggle button style

--- js-experiments/jamming-js/sound.js
+++ js-experiments/jamming-js/sound.js
@@ -30,6 +30,7 @@ class Sound {
     this.gainNode.gain.exponentialRampToValueAtTime(0.01, time + 1);
     this.oscillator.stop(time + 1);
   }
+
 }
 let context = new (window.AudioContext || window.webkitAudioContext)();
 function changeSoundType() {
@@ -46,7 +47,7 @@ let composedIndex = 0;
 let maxComposedIndex = 0;
 let composedNotesArea = document.getElementById("composedNotesArea");
 let composedButton = document.getElementsByClassName("note");
-let column = document.getElementsByClassName("column");
+let composeSection = document.getElementsByClassName("compose-section");
 let mainWrapper = document.getElementById("mainWrapper");
 let noteButtonsid = document.getElementById("noteButtons");
 
@@ -64,34 +65,50 @@ let notes = {
 // note.init();
 class Note {
   constructor() {
-    this.noteButtons = document.createElement("button");
+    this.noteButtons = document.createElement("div");
     this.noteButtons.style.padding = "20px";
     this.noteButtons.className = "note";
     this.noteButtons.style.margin = "5px";
-    
+    this.isClicked = false;
   }
 }
 
 class ColumnNote{
   constructor(){
-
+    this.column = document.createElement('div');
+    this.column.className='column';
+    composeSection[0].appendChild(this.column);
     for (const prop in notes) {
       let note = new Note();
       note.noteButtons.innerHTML = notes[prop];
       note.noteButtons.value = prop;
-      column[0].appendChild(note.noteButtons);
+      this.column.appendChild(note.noteButtons);
       note.noteButtons.addEventListener("click", function () {
-        let note = this.value;
-        composedNotes.push(note);
-        composedHertz.push(notesCollection[note]);
-        maxComposedIndex++;
-        composedNotesArea.value = composedNotes;
+        note.isClicked = !note.isClicked;
+        if(note.isClicked){
+          let note = this.value;
+          let hertzIndex = notesCollection[note];
+          // console.log(hertzIndex);
+          composedNotes.push(note);
+          composedHertz.push(notesCollection[note]);
+          maxComposedIndex++;
+          composedNotesArea.value = composedNotes;
+        }else{
+          composedNotes.splice(composedNotes.indexOf(this.value),1);
+          composedHertz.splice(composedHertz.indexOf(notesCollection[this.value]),1);
+          // console.log(this.value);
+          composedIndex--;
+          console.log(composedHertz);
+          console.log(composedNotes);
+          composedNotesArea.value = composedNotes;
+        
+        }
       });
     }
   }
 }
 let columnNote = new ColumnNote;
-
+let colunNote2 = new ColumnNote;
 // for (const prop in notes) {
 //   let note = new Note();
 //   note.noteButtons.innerHTML = notes[prop];
@@ -110,10 +127,9 @@ for (let i = 0; i < composedButton.length; i++) {
   composedButton[i].addEventListener("click", function () {
     if (this.classList.contains("note")) {
       // this.classList.remove('selected');
-      this.classList.add("selected");
-    } else if (this.classList.contains("selected")) {
-      this.classList.remove("selected");
-    }
+      this.classList.toggle("selected");
+    } 
+   
   });
 }
 
@@ -139,10 +155,31 @@ function stopScale() {
   note.stop(0);
 }
 
-setInterval(checkNotesChosen, 1000);
+let composeSpeed = function(tempo){
+  console.log('tempo',tempo/60*1000);
+  setInterval(checkNotesChosen, tempo/60*1000);
+
+  // console.log(setInterval(checkNotesChosen, tempo));
+};
 
 function checkNotesChosen() {
   if (maxComposedIndex > 0) {
     return playScale();
   }
 }
+tempoSlider = document.getElementById('tempo');
+tempoSlider.min = 10;
+tempoSlider.max = 200;
+tempoSlider.value=60;
+tempoSlider.step=5;
+
+tempoSlider.addEventListener("change",function () {
+  // console.log("slider",sendTempoValue);
+  sendTempoValue = tempoSlider.value;
+  composeSpeed(sendTempoValue);
+});
+function changeTempoValue(){
+  let sendTempoValue = tempoSlider.value;
+  composeSpeed(sendTempoValue);
+}
+// document.onload=changeTempoValue();
