e366449596f2567601200726897685bffed25b43
sbimochan <bmochan@gmail.com>
2018-02-20 16:35:27 +0545

    solve play issue and design

--- jamming-js/app.js
+++ jamming-js/app.js
@@ -14,28 +14,31 @@ const preObject = document.getElementById('composeName');
 const songListDiv = document.getElementById('songList');
 // create reference
 const dbRefObject = firebase.database().ref().child('composition');
-
+let ul = document.createElement('ul');
+ul.style="list-style-type: none"
+songListDiv.appendChild(ul);
 // sync object changes with on menthod
 // snap is data snapshot 
 dbRefObject.on('value', snap => {
   const numOfSong = snap.numChildren()
   if (numOfSong !== 0) {
     for (let i = 0; i < numOfSong; i++) {
+      let li = document.createElement('li');
       let newSong = document.createElement('button');
+      newSong.className = 'songs';
       newSong.innerHTML = Object.keys(snap.val())[i];
       newSong.value = Object.values(snap.val())[i].song;
       linebreak = document.createElement('br');
       newSong.appendChild(linebreak);
-      songListDiv.appendChild(newSong);
+      li.appendChild(newSong);
+      ul.appendChild(li);
       newSong.addEventListener('click', () => {
         let results = JSON.parse(newSong.value);
         results.forEach((result) => {
           let column = new ColumnNote(result.composedHertzArray, result.waveform, result.noteTime, result.noteTimeLength);
           columnNotesArray.push(column);
-            setTimeout(playComposition, 100);
-            console.log(playComposition);
-            
         });
+        setTimeout(playComposition, 100);
       })
     }
   }
