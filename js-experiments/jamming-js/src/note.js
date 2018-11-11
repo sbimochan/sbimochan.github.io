63977b9cbb59be223125bcf2e7bbbe4983629426
sbimochan <bmochan@gmail.com>
2018-11-12 01:03:19 +0545

    Remove secondary jamming-js

--- js-experiments/jamming-js/src/note.js
+++
@@ -1,9 +0,0 @@
-export class Note {
-  constructor() {
-    this.noteButtons = document.createElement('div');
-    this.noteButtons.style.padding = '10px';
-    this.noteButtons.className = 'note';
-    this.noteButtons.style.margin = '5px';
-    this.isClicked = false;
-  }
-}
